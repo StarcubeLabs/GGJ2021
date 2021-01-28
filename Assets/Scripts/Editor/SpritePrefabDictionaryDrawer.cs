3aeacfbe946132f306c9a558ad1d68f1d5258229
AnonymousRandomPerson <chenghanngan.us@gmail.com>
2021-01-28 00:39:51 -0500

    Added tilemap-to-model script

---
+++ Assets/Scripts/Editor/SpritePrefabDictionaryDrawer.cs
@@ -0,0 +1,6 @@
+﻿using UnityEditor;
+
+[CustomPropertyDrawer(typeof(SpritePrefabDictionary))]
+public class SpritePrefabDictionaryDrawer : SerializableDictionaryPropertyDrawer
+{
+}
---
+++ Assets/Scripts/Tilemap/Tilemap3D.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Starjam
+{
+    /// <summary>
+    /// Maps a tilemap to model prefabs.
+    /// </summary>
+    public class Tilemap3D : MonoBehaviour
+    {
+
+        [SerializeField]
+        private SpritePrefabDictionary spritesToPrefabs;
+        private List<GameObject> instantiatedPrefabs = new List<GameObject>();
+
+        void Start()
+        {
+            createPrefabs();
+            GetComponent<TilemapRenderer>().enabled = false;
+        }
+
+        private void createPrefabs()
+        {
+            Tilemap tileMap = GetComponent<Tilemap>();
+            BoundsInt cellBounds = tileMap.cellBounds;
+            for (int x = cellBounds.xMin; x < cellBounds.xMax; x++)
+            {
+                for (int y = cellBounds.yMin; y < cellBounds.yMax; y++)
+                {
+                    Vector3Int cellPos = new Vector3Int(x, y, 0);
+                    Sprite sprite = tileMap.GetSprite(cellPos);
+                    if (sprite != null)
+                    {
+                        GameObject prefab = spritesToPrefabs[sprite];
+                        if (prefab == null)
+                        {
+                            Debug.LogError("Missing prefab for tile " + sprite);
+                        }
+                        GameObject prefabInstance = Instantiate(prefab, tileMap.GetCellCenterWorld(cellPos),
+                            prefab.transform.rotation * tileMap.GetTransformMatrix(cellPos).rotation);
+                        instantiatedPrefabs.Add(prefabInstance);
+                    }
+                }
+            }
+        }
+    }
+
+}
