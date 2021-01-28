8cd677d8b6bbb8f10c242d28f933facdb77a903b
nshooter <nick.shooter@gmail.com>
2021-01-27 23:50:49 -0500

    Reticule

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -10,6 +10,13 @@
 
         public float movementSpeed = 5f;
         private Vector2 move = new Vector2();
+        private Vector2 lookDirection = new Vector2();
+        private float reticuleDistance = 0.3f;
+        private bool facingRight = true;
+
+        public GameObject playerSprites;
+        private Vector3 originalSpritesScale;
+        public GameObject reticule;
 
         private Rigidbody2D rb;
 
@@ -29,16 +36,54 @@
         void Start()
         {
             rb = GetComponentInChildren<Rigidbody2D>();
+            originalSpritesScale = playerSprites.transform.localScale;
         }
 
         // Update is called once per frame
         void Update()
         {
+            Move();
+            AimReticule();
+        }
+
+        private void Move()
+        {
             move.x = RewiredPlayerInputManager.instance.GetHorizontalMovement();
-            if (move.magnitude > 0)
+            if (move.x != 0)
             {
-                rb.velocity = move.normalized * movementSpeed;
+                if (move.x > 0)
+                {
+                    playerSprites.transform.localScale = originalSpritesScale;
+                    facingRight = true;
+                }
+                else
+                {
+                    playerSprites.transform.localScale = new Vector3(-originalSpritesScale.x, originalSpritesScale.y, originalSpritesScale.z);
+                    facingRight = false;
+                }
+                rb.velocity = new Vector2(move.x * movementSpeed, rb.velocity.y);
             }
+        }
+
+        private void AimReticule()
+        {
+            lookDirection = new Vector2(RewiredPlayerInputManager.instance.GetHorizontalMovement2(), RewiredPlayerInputManager.instance.GetVerticalMovement2());
+            if (lookDirection != Vector2.zero)
+            {
+                lookDirection = lookDirection.normalized * reticuleDistance;
+            }
+            else
+            {
+                if (facingRight)
+                {
+                    lookDirection = new Vector2(reticuleDistance, 0f);
+                }
+                else
+                {
+                    lookDirection = new Vector2(-reticuleDistance, 0f);
+                }
+            }
+            reticule.transform.localPosition = lookDirection;
         }
     }
 }
