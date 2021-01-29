0b625d5487bb76af3fb2f0ff5e45614e80fe537b
aahvocado <dan.dan.makes.stuff@gmail.com>
2021-01-28 20:28:41 -0800

    first pass at generating health icons with health bar

---
+++ Assets/Scripts/UI/HealthBarHandler.cs
@@ -0,0 +1,41 @@
+﻿namespace GGJ2021.UserInterface
+{
+
+  using System.Collections;
+  using System.Collections.Generic;
+  using UnityEngine;
+
+  public class HealthBarHandler : MonoBehaviour
+  {
+
+      public int healthMax;
+      public int healthCurr;
+
+      public GameObject healthIcon;
+      private float iconWidth = 25;
+
+      // Start is called before the first frame update
+      void Start()
+      {
+        // init values
+        healthCurr = healthMax;
+
+        this.InstantiateIcons();
+      }
+
+      void InstantiateIcons() {
+        for (int ii=0; ii<healthMax; ii++) {
+          InstantiateIcon(ii);
+        }
+      }
+
+      void InstantiateIcon(int idx) {
+        GameObject icon = Instantiate(healthIcon, transform);
+
+        Vector3 newPosition = transform.position;
+        newPosition.x += idx * (iconWidth + 10);
+
+        icon.transform.position = newPosition;
+      }
+  }
+}
