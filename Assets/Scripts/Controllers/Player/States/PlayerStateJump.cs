28bea82271b66e1cee83f81ff54480a9700dddb1
nshooter3 <nick.shooter@gmail.com>
2021-01-28 22:29:47 -0500

    Jumpman (#11)
    * Made funny man go bounce
    
    * updated jump params

---
+++ Assets/Scripts/Controllers/Player/States/PlayerStateJump.cs
@@ -0,0 +1,43 @@
+﻿namespace GGJ2021
+{
+    public class PlayerStateJump : PlayerState
+    {
+        private float jumpTimer;
+
+        public PlayerStateJump(PlayerController controller) : base(controller)
+        {
+            stateName = "PlayerStateJump";
+            jumpTimer = playerController.config.jumpTime;
+        }
+
+        protected override void Enter(){ }
+
+        public override void OnUpdate(float time)
+        {
+            base.OnUpdate(time);
+
+            jumpTimer -= time;
+            if (jumpTimer <= 0)
+            {
+                ableToExit = true;
+                nextState = new PlayerStateIdle(playerController);
+            }
+
+            if (RewiredPlayerInputManager.instance.R1Down())
+            {
+                playerController.hamsterManager.ShootProjectile(playerController.transform.position, playerController.lookDirection.normalized * playerController.config.ProjectileSpeed);
+            }
+
+            playerController.playerPhysics.CalculateVelocity(playerController.config.MaxSpeed, playerController.config.MaxAcceleration);
+            playerController.playerPhysics.ApplyJump(playerController.config.jumpSpeed);
+        }
+
+        public override void OnFixedUpdate()
+        {
+            playerController.playerPhysics.ApplyVelocity(playerController.config.MaxSpeed);
+            base.OnFixedUpdate();
+        }
+
+        public override void OnExit() { }
+    }
+}
