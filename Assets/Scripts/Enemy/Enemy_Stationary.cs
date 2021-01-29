f08553d30f6c0badf4c49e38dc304b01bb21a766
Chris Tansey <ominous.gamedev@gmail.com>
2021-01-28 23:20:34 -0500

    namespace added to enemy_stationary

--- Assets/Scripts/Enemy/Enemy_Stationary.cs
+++ Assets/Scripts/Enemy/Enemy_Stationary.cs
@@ -2,17 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_Stationary : Enemy_Base
+namespace GGJ2020.Enemy
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Enemy_Stationary : Enemy_Base
     {
-        
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
