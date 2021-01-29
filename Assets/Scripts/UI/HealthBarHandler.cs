e59be1fad05016c1613bb3adf37e003d1f125bf2
aahvocado <dan.dan.makes.stuff@gmail.com>
2021-01-28 20:54:45 -0800

    implement `IncrementMaxHealth()`

--- Assets/Scripts/UI/HealthBarHandler.cs
+++ Assets/Scripts/UI/HealthBarHandler.cs
@@ -9,6 +9,7 @@
   {
 
       public int healthMax;
+
       public int healthCurr;
 
       public GameObject healthIcon;
@@ -23,19 +24,32 @@
         this.InstantiateIcons();
       }
 
-      void InstantiateIcons() {
+      public void InstantiateIcons() {
         for (int ii=0; ii<healthMax; ii++) {
           InstantiateIcon(ii);
         }
       }
 
-      void InstantiateIcon(int idx) {
+      public void InstantiateIcon(int idx) {
         GameObject icon = Instantiate(healthIcon, transform);
 
         Vector3 newPosition = transform.position;
         newPosition.x += idx * (iconWidth + 10);
 
         icon.transform.position = newPosition;
+      }
+
+      public void IncrementMaxHealth(int amount) {
+        int newMax = healthMax + amount;
+
+        // first create additional icons
+        for (int ii=healthMax; ii<newMax; ii++) {
+          InstantiateIcon(ii);
+        }
+
+        // then we can add health, can do curr=max another time
+        healthMax = newMax;
+        healthCurr = healthCurr + amount;
       }
   }
 }
