57924dd503fae7ea9f64c8b2f4d85ff6490c90df
Ominous <cmt922@gmail.com>
2021-01-29 07:45:25 -0500

    Post Tahri Fixes
    + EnemyBehavior Restructure
    + eliminated various interface classes in favore of inheritance hierarchy
    + fixed namespaces
    + fixed code compilation errors

---
+++ Assets/Scripts/Enemy/NPC_Base.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+
+namespace GGJ2021.Enemy
+{
+
+    public enum Hosility
+    {
+        ENEMY,
+        FRIENDLY,
+        NEUTRAL
+    }
+
+    public enum ActorState
+    {
+        PAUSED,
+        IDLE,
+        PURSUIT,
+        ATTACKING,
+        RETURNTOPOST
+    }
+
+    /// <summary>
+    /// specifies the derived class must define hit
+    /// </summary>
+    interface IHittable
+    {
+        void hit();
+
+        void hit(int attackValue);
+    }
+
+    public class NPC_Base :  FlippableCharacter
+    {
+        protected int maxhealth;     //maxhealth is a float in case of peeshooter dmg
+        protected int health;        //current health of enemy
+        protected ActorState EnemyState;    // controls the enemey state
+
+        /// <summary>
+        /// Called by the NPC_Pool Singleton Class when NPC_Pool is asked to pause all NPCs & bullets
+        /// </summary>
+        public void pause()
+        {
+            // TO DO: Write default definition
+        }
+
+        protected virtual void idle()
+        {
+        }
+        protected virtual void patrol()
+        {
+
+        }
+        protected virtual void combat()
+        {
+
+        }
+
+        protected virtual void pursuit()
+        {
+
+        }
+        protected virtual void returnToPost()
+        {
+
+        }
+        public virtual void spawn()
+        {
+            // NPC_Pool.addToNPCPausePool();
+        }
+        /// <summary>
+        /// Reinitialize location and default constructor values
+        /// </summary>
+        public virtual void respawn()
+        {
+
+        }
+        protected void death()
+        {
+            // TO DO Write default definition
+        }
+    }
+}
+
