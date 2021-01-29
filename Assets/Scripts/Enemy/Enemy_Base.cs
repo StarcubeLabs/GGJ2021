6505877e7f18c0f55dcae9cf820bbf2e52c8c7ce
Chris Tansey <ominous.gamedev@gmail.com>
2021-01-28 23:19:28 -0500

    Update Enemy_Base.cs

--- Assets/Scripts/Enemy/Enemy_Base.cs
+++ Assets/Scripts/Enemy/Enemy_Base.cs
@@ -6,66 +6,68 @@ using UnityEngine;
 
 
 
-
-
-public class Enemy_Base : MonoBehaviour
+namespace GGJ2020.Enemy
 {
-    public float maxhealth; // maxhealth is a float in case of peeshooter dmg
-    public float health;    //current health of enemy
-    public float damage;    //damage dealt to player
-    public float moveSpeed; //move speed for mobile characters
-    public float jellyLevel; 
-    public float coolDown;
-    public GameObject projectile;
-    public List<GameObject> positions;
-    public Collider2D hitbox;
 
-
-
-
-
-    // Start is called before the first frame update
-    void Start()
+    public class Enemy_Base : MonoBehaviour
     {
-        if(positions.Count == 0) // init to static lerping position
+        public float maxhealth; // maxhealth is a float in case of peeshooter dmg
+        public float health;    //current health of enemy
+        public float damage;    //damage dealt to player
+        public float moveSpeed; //move speed for mobile characters
+        public float jellyLevel; 
+        public float coolDown;
+        public GameObject projectile;
+        public List<GameObject> positions;
+        public Collider2D hitbox;
+
+
+
+
+
+        // Start is called before the first frame update
+        void Start()
         {
-            positions[0].transform.position = this.gameObject.transform.position;
-            positions[1].transform.position = this.gameObject.transform.position;
-        }      
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-        this.gameObject.transform.position = Vector3.Lerp(positions[0].transform.position,   
-            positions[1].transform.position, Mathf.PingPong(Time.time * moveSpeed, 1.0f)); // Lerp between points
-
-        if(health == 0)
-        {
-            Destroy(this.gameObject, 0.1f);
+            if(positions.Count == 0) // init to static lerping position
+            {
+                positions[0].transform.position = this.gameObject.transform.position;
+                positions[1].transform.position = this.gameObject.transform.position;
+            }      
         }
-       
 
-    }
+        // Update is called once per frame
+        void Update()
+        {
 
-    private void OnCollisionEnter(Collision collision)
-    {
-       //add damage method on collision
-    }
+            this.gameObject.transform.position = Vector3.Lerp(positions[0].transform.position,   
+                positions[1].transform.position, Mathf.PingPong(Time.time * moveSpeed, 1.0f)); // Lerp between points
 
-    public virtual void Move()
-    {
-       
-    }
+            if(health == 0)
+            {
+                Destroy(this.gameObject, 0.1f);
+            }
 
-    public virtual void Attack()
-    {
 
-    }
+        }
 
-    public virtual void TakeDamage(float damage)
-    {
-        health -= damage;
+        private void OnCollisionEnter(Collision collision)
+        {
+           //add damage method on collision
+        }
+
+        public virtual void Move()
+        {
+
+        }
+
+        public virtual void Attack()
+        {
+
+        }
+
+        public virtual void TakeDamage(float damage)
+        {
+            health -= damage;
+        }
     }
 }
