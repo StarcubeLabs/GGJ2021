949a25606e8765ec1124d33391a55ad1c7de0a55
nshooter3 <nick.shooter@gmail.com>
2021-01-29 01:37:01 -0500

    Dash (#15)
    * first pass at dash
    
    * more dash stuff
    
    * removed uneccessary stuff

---
+++ Assets/Scripts/Controllers/Player/DashVisualIndicator.cs
@@ -0,0 +1,28 @@
+﻿namespace GGJ2021
+{
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class DashVisualIndicator : MonoBehaviour
+    {
+        public List<SpriteRenderer> sprites;
+        public Color defaultColor, dimColor;
+
+        private void Update()
+        {
+            ToggleColor(PlayerController.instance.playerPhysics.canDash);
+        }
+
+        public void ToggleColor(bool canDash)
+        {
+            if (canDash)
+            {
+                sprites.ForEach(p => p.color = defaultColor);
+            }
+            else
+            {
+                sprites.ForEach(p => p.color = dimColor);
+            }
+        }
+    }
+}
