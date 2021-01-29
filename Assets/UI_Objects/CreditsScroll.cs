1a466c3f6f39725fec298f81bb3d00479b4feb5f
Daniel Xiao <dan.dan.makes.stuff@gmail.com>
2021-01-28 17:12:43 -0800

    Credits (#8)
    * create credits scene
    
    * can navigate to credits from menu
    
    * add button to return to menu
    
    * add brian

---
+++ Assets/UI_Objects/CreditsScroll.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CreditsScroll : MonoBehaviour
+{
+    public int speed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.up * Time.deltaTime * speed);
+    }
+}
