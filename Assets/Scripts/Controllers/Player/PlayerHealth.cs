85c52f6ff2fa8afac2714ba76ce9fef254fe1b90
nshooter <nick.shooter@gmail.com>
2021-01-30 03:38:48 -0500

    Player now takes damage

---
+++ Assets/Scripts/Controllers/Player/PlayerHealth.cs
@@ -0,0 +1,51 @@
+﻿namespace GGJ2021
+{
+    using GGJ2021.UI;
+    using UnityEngine;
+
+    public class PlayerHealth
+    {
+        private int curHealth, maxHealth = 3;
+        private CollisionWrapper playerHurtboxColliderWrapper;
+
+        public PlayerHealth(PlayerController controller, CollisionWrapper playerHurtboxColliderWrapper)
+        {
+            curHealth = maxHealth;
+            this.playerHurtboxColliderWrapper = playerHurtboxColliderWrapper;
+            this.playerHurtboxColliderWrapper.AssignFunctionToTriggerEnterDelegate(OnCollision);
+        }
+
+        public void TakeDamage(int damage)
+        {
+            curHealth -= damage;
+            StatBarManager.instance.SetCurr(curHealth);
+            if (curHealth <= 0)
+            {
+                Die();
+            }
+        }
+
+        public void RestoreHealth(int healing)
+        {
+            curHealth = Mathf.Min(maxHealth, curHealth + healing);
+            StatBarManager.instance.SetCurr(curHealth);
+        }
+
+        public void IncreaseMaxHealth(int amount)
+        {
+            maxHealth += amount;
+            curHealth = maxHealth;
+            StatBarManager.instance.SetMax(maxHealth);
+        }
+
+        private void Die()
+        {
+            //TODO: Die.
+        }
+
+        public void OnCollision(Collider2D col)
+        {
+            TakeDamage(1);
+        }
+    }
+}
