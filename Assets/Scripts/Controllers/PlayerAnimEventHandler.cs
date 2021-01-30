52bc29800fd6c2749d4a6ff2bf36006ebf56d9e9
Spex130 <Gamerguy56@bellsouth.net>
2021-01-30 12:49:44 -0500

    Modified Hurt, Idle, Jump, and Walk to have animation events. Added script to add animation events to the PC animator. Added Death and AstroHurtAcid animations.

---
+++ Assets/Scripts/Controllers/PlayerAnimEventHandler.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEditor.Animations;
+using AnimatorController = UnityEditor.Animations.AnimatorController;
+
+public class PlayerAnimEventHandler : MonoBehaviour
+{
+    public AnimatorController AstroAnimController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayStepSound()
+    {
+
+    }
+
+    public void PlayJumpSound()
+    {
+
+    }
+
+    public void PlayPlayerDamageSound()
+    {
+
+    }
+
+    public void PlayAcidDamagedSound()
+    {
+
+    }
+
+    public void PlayPlayerDeathSound()
+    {
+
+    }
+}
