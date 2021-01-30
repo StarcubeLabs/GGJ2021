97ef552369341ca660d5085b24fdde95e158f6e8
Chris Tansey <ominous.gamedev@gmail.com>
2021-01-29 23:40:09 -0500

    EnemyPool Spawning Implemented (#26)
    + Enemy Pool singleton added to be commanded to spawn an enemy type
    
    + EnemySpritePrefabs acts as an Enemy prefab Library
    
    + Spawn() inheritance finalized
    
    + Init() inheriteance finalized
    
    + Goomba implemented as spawnable enemy (AI and Player interaction not included)

---
+++ Assets/Scripts/Enemy/EnemySpriteHolder_Base.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GGJ2021.Enemy
+{
+    public class EnemySpriteHolder_Base : MonoBehaviour
+    {
+        public Enemy_NPC[] prefabs;
+
+        private void Start()
+        {
+            Vector3 playerLoc = FindObjectOfType<PlayerController>().transform.position;
+            NPCPool_Base.Instance.SpawnEnemy(playerLoc, EnemyTypes.GOOMBA);
+        }
+    }
+}
