b179eb168a26400072a25c9a650c73c6683944f3
nshooter3 <nick.shooter@gmail.com>
2021-01-30 23:36:37 -0500

    Added ball pipes (#65)

---
+++ Assets/Models/TileSets/Pipe/TransitPipeEntrance.cs
@@ -0,0 +1,22 @@
+﻿namespace GGJ2021
+{
+    using UnityEngine;
+
+    public class TransitPipeEntrance : MonoBehaviour
+    {
+        public Pipe.StartingPoint pipeType;
+        public Pipe pipe;
+        public CollisionWrapper colWrapper;
+
+        private void Start()
+        {
+            colWrapper.AssignFunctionToTriggerEnterDelegate(OnCollision);
+            colWrapper.AssignFunctionToTriggerStayDelegate(OnCollision);
+        }
+
+        public void OnCollision(Collider2D col)
+        {
+            PlayerController.instance.OnPipeCollision(pipe, pipeType);
+        }
+    }
+}
