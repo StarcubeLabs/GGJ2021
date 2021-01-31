44d1a84bf479022ecb762307ced82c91f20daf69
nshooter <nick.shooter@gmail.com>
2021-01-31 02:55:23 -0500

    first pass at hamster visual

---
+++ Assets/Scripts/Controllers/Player/GrappleHamManager.cs
@@ -0,0 +1,26 @@
+﻿namespace GGJ2021
+{
+    using UnityEngine;
+
+    public class GrappleHamManager : MonoBehaviour
+    {
+        public SpriteRenderer ham;
+        private bool grappleVisible;
+        private bool grapplegrabbed;
+
+        // Update is called once per frame
+        void Update()
+        {
+            grapplegrabbed = PlayerController.instance.playerPhysics.isGrappling;
+            grappleVisible = PlayerController.instance.playerGrappleManager.GrappleActive() || grapplegrabbed;
+
+            ham.transform.position = PlayerController.instance.playerGrappleManager.GetGrappleEndPos();
+
+            Vector3 rotatedVectorToTarget = Quaternion.Euler(0, 0, 180) * -PlayerController.instance.playerGrappleManager.GetGrappleDirection();
+            Quaternion targetRotation = Quaternion.LookRotation(Vector3.forward, rotatedVectorToTarget);
+            ham.transform.rotation = targetRotation;
+
+            ham.enabled = grappleVisible;
+        }
+    }
+}
