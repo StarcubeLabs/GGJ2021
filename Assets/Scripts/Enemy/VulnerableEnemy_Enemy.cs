88f8083762b079fce4f3bb3955561dc7f20d0be4
Ominous <cmt922@gmail.com>
2021-01-31 05:58:01 -0500

    Commented VulnerableEnemy
    + This handles all enemies that can be hit/damaged by the player

--- Assets/Scripts/Enemy/VulnerableEnemy_Enemy.cs
+++ Assets/Scripts/Enemy/VulnerableEnemy_Enemy.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 namespace GGJ2021.Enemy
 {
+    // This handles every enemy that can receive damage from the player
     public abstract class VulnerableEnemy_Enemy : Enemy_NPC, IHittable
     {
         /// <summary>
@@ -11,22 +12,29 @@ namespace GGJ2021.Enemy
         /// </summary>
         public CollisionWrapper hurtboxCollisionWrapper;
 
+        /// <summary>
+        /// Default damage to the enemy, always -1
+        /// </summary>
         public virtual void hit()
         {
-            // TO DO: Write default definition
-
             healthChange();
         }
 
+        /// <summary>
+        /// Can specify damage to the enemy. Signed values matter.
+        /// </summary>
+        /// <param name="attackValue"></param>
         public virtual void hit(int attackValue)
         {
-            // TO DO: Write default definition
             if (attackValue > 0)
                 attackValue = -attackValue;
             healthChange(attackValue);
         }
 
-
+        /// <summary>
+        /// Handles the HurtboxCollisionWrapper Assignment
+        /// </summary>
+        /// <param name="v"></param>
         protected virtual void Init(Vector2 v)
         {
             base.Init(v);
@@ -55,6 +63,10 @@ namespace GGJ2021.Enemy
 #endif
         }
 
+        /// <summary>
+        /// Nick's Delegate for collision
+        /// </summary>
+        /// <param name="col"></param>
         public void OnCollision(Collider2D col)
         {
             if (col.gameObject.layer == LayerMask.NameToLayer("PlayerHitbox"))
