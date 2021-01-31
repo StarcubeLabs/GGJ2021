7dc890f3a77d992650aa4661a9d265f8a8112cbe
Mitch McClellan <mitchelljmcclellan@gmail.com>
2021-01-31 00:41:44 -0500

    Changed parallax prefabs. Added editable layer velocity scales.

--- Assets/Scripts/Effects/Parallax.cs
+++ Assets/Scripts/Effects/Parallax.cs
@@ -7,6 +7,7 @@
     {
         public Transform target;
         public InfiniteScroll[] layers;
+        public float[] layersVelocityScales;
         public float parallaxEffectMulitplyer;
 
         private void Start()
@@ -14,6 +15,7 @@
             Assert.IsNotNull(target);
             for (int i = 0; i < layers.Length; i++)
                 Assert.IsNotNull(layers[i]);
+            Assert.AreEqual(layers.Length, layersVelocityScales.Length);
         }
 
         private void Update()
@@ -27,9 +29,9 @@
             }
 
             Vector2 vel = (this.transform.position - target.position).normalized * parallaxEffectMulitplyer;
-            Vector2 foreground = vel * .25f;
-            Vector2 middleground = vel * .5f;
-            Vector2 background = vel;
+            Vector2 foreground = vel * layersVelocityScales[0];
+            Vector2 middleground = vel * layersVelocityScales[1];
+            Vector2 background = vel * layersVelocityScales[2];
 
             layers[0].transform.localPosition = new Vector3(foreground.x, foreground.y, layers[0].transform.localPosition.z);
             layers[1].transform.localPosition = new Vector3(middleground.x, middleground.y, layers[1].transform.localPosition.z);
