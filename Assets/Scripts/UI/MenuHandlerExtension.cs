b66414033f21d9fb0d6dee9f0300598f1d77fa1e
Daniel Xiao <dan.dan.makes.stuff@gmail.com>
2021-01-31 15:01:09 -0800

    Controllermenu (#125)
    * create menu handler extension to pick menu selection
    
    * move portrait down on hud

---
+++ Assets/Scripts/UI/MenuHandlerExtension.cs
@@ -0,0 +1,33 @@
+﻿namespace GGJ2021.UI
+{
+    using UnityEngine;
+    using UnityEngine.EventSystems;
+
+    public class MenuHandlerExtension : MonoBehaviour
+    {
+        public GameObject topSelection;
+        public GameObject bottomSelection;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (EventSystem.current.currentSelectedGameObject != null) {
+                return;
+            }
+
+            float inputChangeY = RewiredPlayerInputManager.instance.GetVerticalMovement();
+            if (inputChangeY > 0) {
+                EventSystem.current.SetSelectedGameObject(bottomSelection);
+            }
+            if (inputChangeY < 0) {
+                EventSystem.current.SetSelectedGameObject(topSelection);
+            }
+        }
+    }
+}
