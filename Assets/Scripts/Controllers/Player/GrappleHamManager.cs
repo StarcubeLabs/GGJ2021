2e0a193812b30f54ffcc61bfa3e81513f653a612
Spex130 <Gamerguy56@bellsouth.net>
2021-01-31 03:26:15 -0500

    Set up Grapple Ham. Also, modified rotation to be LateUpdate.

--- Assets/Scripts/Controllers/Player/GrappleHamManager.cs
+++ Assets/Scripts/Controllers/Player/GrappleHamManager.cs
@@ -16,11 +16,16 @@
 
             ham.transform.position = PlayerController.instance.playerGrappleManager.GetGrappleEndPos();
 
+
+
+            ham.enabled = grappleVisible;
+        }
+
+        private void LateUpdate()
+        {
             Vector3 rotatedVectorToTarget = Quaternion.Euler(0, 0, 180) * -PlayerController.instance.playerGrappleManager.GetGrappleDirection();
             Quaternion targetRotation = Quaternion.LookRotation(Vector3.forward, rotatedVectorToTarget);
             ham.transform.rotation = targetRotation;
-
-            ham.enabled = grappleVisible;
         }
     }
 }
