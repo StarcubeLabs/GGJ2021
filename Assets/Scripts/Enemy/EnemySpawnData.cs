eaf63601f8e579611c94823cd4fc4ad1dbd4b3ec
Jonathan Hunter <hunterjonathant@gmail.com>
2021-01-31 06:51:19 -0800

    enemy spawning (#95)

---
+++ Assets/Scripts/Enemy/EnemySpawnData.cs
@@ -0,0 +1,10 @@
+﻿namespace GGJ2021.Enemy
+{
+    using UnityEngine;
+
+    public class EnemySpawnData : MonoBehaviour
+    {
+        public Transform[] SpawnPoints;
+        public Management.EnemyManager.EnemyType[] Types;
+    }
+}
