e669ba054ceceb3d46e6957bfcca7c0914fc35f4
nshooter3 <nick.shooter@gmail.com>
2021-01-31 02:29:11 -0500

    Pipe vfx branch (#74)
    * Set up some fancy tube mats and made a cap prefab for the tube ends.
    
    * Missed material.
    
    * pipe tweaks
    
    * Changes to various shaders so they interact with HDR properly.
    
    * fixed pipe enter bug
    
    * removed log
    
    Co-authored-by: Spex130 <Gamerguy56@bellsouth.net>

---
+++ Assets/Scripts/Effects/DustToggle.cs
@@ -0,0 +1,28 @@
+﻿namespace GGJ2021
+{
+    using UnityEngine;
+
+    public class DustToggle : MonoBehaviour
+    {
+        public ParticleSystem particles;
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (PlayerStats.instance.HasAbility(Ability.Dash))
+            {
+                if (!particles.isEmitting)
+                {
+                    particles.Play();
+                }
+            }
+            else
+            {
+                if (particles.isEmitting)
+                {
+                    particles.Stop();
+                }
+            }
+        }
+    }
+}
