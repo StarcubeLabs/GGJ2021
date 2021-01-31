3409fdfcf8d4c3375394775838711ab98b3f12ac
Jonathan Hunter <hunterjonathant@gmail.com>
2021-01-31 10:18:55 -0800

    added tutorial support (#106)

---
+++ Assets/Scripts/UI/Tutorial.cs
@@ -0,0 +1,33 @@
+﻿namespace GGJ2021.UI
+{
+    using UnityEngine;
+
+    public class Tutorial : MonoBehaviour
+    {
+        public GameObject tutorial;
+        public bool checkForAbility;
+        public Ability ability;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (checkForAbility)
+            {
+                if (!PlayerStats.instance.HasAbility(ability))
+                    return;
+            }
+
+            tutorial.SetActive(true);
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (checkForAbility)
+            {
+                if (!PlayerStats.instance.HasAbility(ability))
+                    return;
+            }
+
+            tutorial.SetActive(false);
+        }
+    }
+}
