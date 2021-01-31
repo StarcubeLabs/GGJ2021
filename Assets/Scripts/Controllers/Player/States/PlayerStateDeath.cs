584a522921b342e8b5f8fbe6105d1e740fcd04fc
AnonymousRandomPerson <chenghanngan.us@gmail.com>
2021-01-31 09:42:33 -0500

    Respawn after death

--- Assets/Scripts/Controllers/Player/States/PlayerStateDeath.cs
+++ Assets/Scripts/Controllers/Player/States/PlayerStateDeath.cs
@@ -1,17 +1,29 @@
-﻿namespace GGJ2021
+using GGJ2021.Management;
+
+namespace GGJ2021
 {
     public class PlayerStateDead : PlayerState
     {
+        private const float DEATH_TIME = 1.5f;
+        private float deathTimer;
+
         public PlayerStateDead(PlayerController controller) : base(controller) { stateName = "PlayerStateDead"; }
 
         protected override void Enter()
         {
+            deathTimer = DEATH_TIME;
             FmodFacade.instance.CreateAndRunOneShotFmodEvent("player_death");
         }
 
         public override void OnUpdate(float time)
         {
             base.OnUpdate(time);
+            deathTimer -= time;
+            if (deathTimer <= 0)
+            {
+                PlayerController.instance.playerHealth.RestoreHealthToFull();
+                PlayerController.instance.GetComponentInChildren<SceneTransition>().Transition();
+            }
         }
 
         public override void OnFixedUpdate()
